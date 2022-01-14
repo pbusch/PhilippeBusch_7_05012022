2edabb4e449f220477446bce295345c8687689aa
Philippe Busch <philippe.busch@gmail.com>
2022-01-14 17:09:10 +0100

    User PUT & GET

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,33 +3,8 @@ const User = db.user;
 const Op = db.Sequelize.Op;
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const passwordValidator = require("password-validator");
 
 exports.signup = (req, res) => {
-  const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
-    String(req.body.email).toLowerCase()
-  );
-
-  if (!emailFormat) {
-    return res.status(400).json({ error: "invalid email" });
-  }
-
-  const nameFormat =
-    /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/.test(
-      String(req.body.name)
-    );
-
-  if (!nameFormat) {
-    return res.status(400).json({ error: "invalid name" });
-  }
-
-  const schema = new passwordValidator();
-  schema.is().min(3);
-
-  if (!schema.validate(req.body.password)) {
-    return res.status(400).json({ error: "invalid password" });
-  }
-
   bcrypt.hash(req.body.password, 10).then((hash) => {
     const user = {
       name: req.body.name,
