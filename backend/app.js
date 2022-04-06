c41deb5eb610b715761e9861b0227aeb7f548188
Philippe Busch <philippe.busch@gmail.com>
2022-04-06 10:18:14 +0200

    Admin Pannel

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,14 @@
 const express = require("express");
 const app = express();
-//const Pool = require("pg").Pool;
 const userRoutes = require("./routes/user");
 const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
 const path = require("path");
 require("dotenv").config();
 const db = require("./models");
+
 db.sequelize.sync();
 
-// ----
-// Pool Postgres
-//const pool = new Pool({
-//  user: "groupomania",
-//  host: "localhost",
-//  database: "groupomania",
-//  password: "aaa",
-//  dialect: "postgres",
-//  port: 5432,
-//});
-//pool.connect((err, client, release) => {
-//  if (err) {
-//    return console.error("Error acquiring client", err.stack);
-//  }
-//  client.query("SELECT NOW()", (err, result) => {
-//    release();
-//   if (err) {
-//      return console.error("Error executing query", err.stack);
-//    }
-//    console.log("Connected to Database !");
-//  });
-//});
-// ---
-
-// contrôles d'accès CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -51,7 +26,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-// routes d'authentification
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
